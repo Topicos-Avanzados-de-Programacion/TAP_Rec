366579e419e2292b0cfe31ca25b2ab40e1cd3512
Brandon <colima_97@hotmail.com>
2017-04-28 20:59:25 -0500

    ActionBar ya funciona

--- app/src/main/java/cry/who/boy/tap_recordatorios/Add_New_Reminder.java
+++ app/src/main/java/cry/who/boy/tap_recordatorios/Add_New_Reminder.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.format.Time;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -161,5 +163,19 @@ public class Add_New_Reminder extends AppCompatActivity implements View.OnClickL
 
         }
     }
-    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
