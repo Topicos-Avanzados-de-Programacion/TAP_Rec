abef5f95a65e4f06f0b13b23c19b56e8c781a747
Brandon Itzcoatl Pérez Sandoval <colima_97@hotmail.com>
2017-05-11 12:57:46 -0500

    Copié la estructura y diseño de la vista Añadir_new_reminder

--- app/src/main/java/cry/who/boy/tap_recordatorios/Editar.java
+++ app/src/main/java/cry/who/boy/tap_recordatorios/Editar.java
@@ -1,13 +1,34 @@
 package cry.who.boy.tap_recordatorios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Editar extends AppCompatActivity {
+    private Toolbar toolbar; //Declarar el Toolbar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar);
+
+        //Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);//Conectamos el Layout al objeto Toolbar
+        setSupportActionBar(toolbar);//Configuración de la barra de herramientas (toolbar)
+        // como la Barra de Acciones (ActionBar) con la llamada de setSupportActionBar()
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.volver_blanco));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View atras) {
+                Intent Atras=new Intent(Editar.this, MainActivity.class);
+                startActivity(Atras);
+                finish();
+            }
+        });
+
     }
 }
